d6b28fdae70260d28551ed8c00fdc9fc8dfb73ec
Denis Filippov <dfilippov@oneincsystems.com>
2019-09-02 15:29:19 +0700

    add Dockerfile

--- finance.go
+++ finance.go
@@ -2,8 +2,100 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
+
+var db *sqlx.DB
+
+type Card struct {
+	Card_ID int64
+	Card_Name string
+	Description string
+}
+
+func GetCards() []Card {
+	cards := []Card{}
+
+	err := db.Select(&cards, "SELECT card_id, card_name, description FROM cards")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return cards
+}
+
+func InsertCard(card *Card) {
+	_, err :=db.NamedExec(`INSERT INTO cards(card_name, description) VALUES (:card_name, :description)`, *card)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func UpdateCardByName(card *Card) {
+	_, err :=db.NamedExec(`UPDATE cards SET description = :description WHERE card_name = :card_name`, *card)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+
+	var err error
+	db, err = sqlx.Connect("postgres", "host=10.115.20.240 user=postgres password=tamplier dbname=budget sslmode=disable")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//cardIns := Card {
+	//	Card_Name: "dumm6666",
+	//	Description: "Dummy card",
+	//}
+	//
+	//db.NamedExec(`INSERT INTO cards(card_name, description) VALUES (:card_name, :description)`, cardIns)
+
+	//card := Card{}
+	//
+	//rows, err := db.Queryx("SELECT card_id, card_name, description FROM cards")
+	//
+	//for rows.Next() {
+	//	err := rows.StructScan(&card)
+	//	if err != nil {
+	//		log.Fatalln(err)
+	//	}
+	//	fmt.Fprintf(w, "%v - %v - %v\n", card.Card_ID, card.Card_Name, card.Description)
+	//}
+
+	//cardIns := Card {
+	//	Card_Name: "TestCard",
+	//	Description: "test changed card",
+	//}
+
+	//InsertCard(&cardIns)
+
+	cards := GetCards()
+
+	for i, crd := range cards {
+		fmt.Fprintf(w, "%v: %v - %v - %v\n", i + 1, crd.Card_ID, crd.Card_Name, crd.Description)
+	}
+
+	fmt.Fprintf(w, "Hello people")
+}
 
 func main() {
 	fmt.Printf("hello there")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", getHandler).Methods("GET")
+
+	http.Handle("/", router)
+	err := http.ListenAndServe(":8082", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/filippovdenis/finance
 
 go 1.12
 
-require github.com/jmoiron/sqlx v1.2.0
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/lib/pq v1.0.0
+)
